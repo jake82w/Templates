fb1335c2ce2399e5701f77ec7d53dfcb3ed0d5d3
Phil Henning <phenning@microsoft.com>
2017-08-15 14:36:07 -0700

    add more debugging.

--- tools/build.cmd
+++ tools/build.cmd
@@ -2,6 +2,9 @@
 
 call %~dp0\EnsureTemplatesEnv.cmd
 
+echo dotnet version:
+dotnet --version
+
 dotnet msbuild %TemplatesRoot%\template_feed\Template.proj /t:Build;Test
 if errorlevel 0 exit /b 0
 
