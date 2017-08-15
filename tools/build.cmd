b84b2baf80090e6b6bd6325186256a89114f181f
Phil Henning <phenning@microsoft.com>
2017-08-15 14:13:26 -0700

    Update tools/build.cmd

--- tools/build.cmd
+++ tools/build.cmd
@@ -2,7 +2,7 @@
 
 call %~dp0\EnsureTemplatesEnv.cmd
 
-msbuild %TemplatesRoot%\template_feed\Template.proj /t:Build;Test
+dotnet msbuild %TemplatesRoot%\template_feed\Template.proj /t:Build;Test
 if errorlevel 0 exit /b 0
 
 :ERROR
